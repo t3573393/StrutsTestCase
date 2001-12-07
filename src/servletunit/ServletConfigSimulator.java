ecbc35202375e3c0fff606e7d4f35f7fc7fe1834
Deryl Seale <deryl@acm.org>
2001-12-07 21:26:56 +0000

    added better docs.  Also added support to pass init parameters to ServletContext

--- src/servletunit/ServletConfigSimulator.java
+++ src/servletunit/ServletConfigSimulator.java
@@ -41,87 +41,99 @@ import java.util.Hashtable;
 
 public class ServletConfigSimulator implements ServletConfig
 {
-    protected Hashtable parameters;
-    private ServletContextSimulator context;
+
+    private Hashtable parameters;
+    private ServletContext context;
 
     public ServletConfigSimulator()
     {
         parameters=new Hashtable();
-        context = new ServletContextSimulator();
+        context = new ServletContextSimulator(parameters);
     }
-        /**
-         * Returns a <code>String</code> containing the value of the
-         * named initialization parameter, or <code>null</code> if
-         * the parameter does not exist.
-         *
-         * <p>The value of an initialization parameter is a single
-         * <code>String</code>, which you must interpret.
-         *
-         *
-         * @param name  a <code>String</code> containing the name
-         *                      of the parameter whose value is requested
-         *
-         * @return              a <code>String</code> representing the value
-         *                      of the parameter
-         *
-         */
 
-        public String getInitParameter(String name)
-        {
-                String as[] = getParameterValues(name);
-                if(as != null)
-                 return as[0];
-                else
-                 return null;
-        }
-        /**
-         * Returns the names of the servlet's initialization parameters
-         * as an <code>Enumeration</code> of <code>String</code> objects,
-         * or an empty <code>Enumeration</code> if the servlet has
-         * no initialization parameters.
-         *
-         * @return              an <code>Enumeration</code> of <code>String</code>
-         *                      objects containing the names of the servlet's
-         *                      initialization parameters
-         *
-         *
-         *
-         */
+    /**
+     * Returns a <code>String</code> containing the value of the
+     * named initialization parameter, or <code>null</code> if
+     * the parameter does not exist.
+     *
+     * @param name      a <code>String</code> specifying the name
+     *                  of the initialization parameter
+     *
+     * @return          a <code>String</code> containing the value
+     *                  of the initialization parameter
+     *
+     */
+    public String getInitParameter(String name)
+    {
+        return (String) parameters.get(name);
+    }
 
-        public Enumeration getInitParameterNames()
-        {
-                return parameters.keys();
-        }
-        public String[] getParameterValues(String s)
-        {
-          return (String[])parameters.get(s);
-        }
-        /**
-         * Returns the {@link ServletContext} object that the server
-         * has passed to this servlet. The <code>ServletContext</code>
-         * object is part of the <code>ServletConfig</code> object this
-         * interface defines.
-         *
-         *
-         * @return              a {@link ServletContext} object, which
-         *                      gives the servlet information about how
-         *                      to interact with the server
-         *
-         * @see         ServletContext
-         *
-         */
+    /**
+     * Returns the names of the servlet's initialization parameters
+     * as an <code>Enumeration</code> of <code>String</code> objects,
+     * or an empty <code>Enumeration</code> if the servlet has
+     * no initialization parameters.
+     *
+     * @return          an <code>Enumeration</code> of <code>String</code>
+     *                  objects containing the names of the servlet's
+     *                  initialization parameters
+     *
+     *
+     *
+     */
+    public Enumeration getInitParameterNames()
+    {
+        return parameters.keys();
+    }
 
-        public ServletContext getServletContext()
-        {
-            return context;
-        }
-        public String getServletName()
-        {
-                return null;
-        }
-        public void setInitParameter(String key,String value)
-        {
-                String [] arr = {value};
-                        parameters.put(key,arr);
-        }
+    /**
+     * Returns a reference to the {@link ServletContext} in which the caller
+     * is executing.
+     *
+     *
+     * @return          a {@link ServletContext} object, used
+     *                  by the caller to interact with its servlet
+     *                  container
+     *
+     * @see             ServletContext
+     *
+     */
+    public ServletContext getServletContext()
+    {
+        return context;
+    }
+
+    /**
+     * Returns the name of this servlet instance.
+     * The name may be provided via server administration, assigned in the
+     * web application deployment descriptor, or for an unregistered (and thus
+     * unnamed) servlet instance it will be the servlet's class name.
+     *
+     * @return          the String "ActionServlet"
+     *
+     *
+     *
+     */
+    public String getServletName()
+    {
+        return "ActionServlet";
+    }
+
+    /**
+     * Sets a named initialization parameter with the supplied
+     * <code>String</code> value.
+     *
+     * @param name      a <code>String</code> specifying the name
+     *                  of the initialization parameter
+     *
+     * @param value     a <code>String</code> value for this initialization
+     *                  parameter
+     *
+     */
+    public void setInitParameter(String key,String value)
+    {
+        parameters.put(key,value);
+    }
+
 }
+
