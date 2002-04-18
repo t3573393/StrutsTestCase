3ab6986d37a38e5bc9b2e296336482c15f4e34ca
Deryl Seale <deryl@acm.org>
2002-04-18 21:51:10 +0000

    moved extraneous tests deeper in source tree to keep from release

---
+++ src/servletunit/struts/tests/TestMessageAction.java
@@ -0,0 +1,43 @@
+//  StrutsTestCase - a JUnit extension for testing Struts actions
+//  within the context of the ActionServlet.
+//  Copyright (C) 2001 Deryl Seale
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You may view the full text here: http://www.gnu.org/copyleft/lesser.txt
+//
+//  If you have any questions or suggestions, you may contact me at
+//  this address: Deryl Seale - deryl@acm.org
+
+package servletunit.struts.tests;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import servletunit.struts.MockStrutsTestCase;
+import java.io.IOException;
+
+public class TestMessageAction extends MockStrutsTestCase {
+
+    public TestMessageAction(String testName) {
+        super(testName);
+    }
+
+    public void testGetResources() {
+        setRequestPathInfo("/testMessage");
+	setServletConfigFile("/WEB-INF/web.xml");
+        actionPerform();
+        verifyForward("success");
+        verifyNoActionErrors();
+    }
+
+
+}
+
