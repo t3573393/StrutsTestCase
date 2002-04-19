bf4f9d5367f40d72f401c5c6a42c4810ce7b8ad2
Deryl Seale <deryl@acm.org>
2002-04-19 22:27:47 +0000

    added token tests

---
+++ src/servletunit/struts/tests/TokenAction.java
@@ -0,0 +1,50 @@
+//  StrutsTestCase - a JUnit extension for testing Struts actions
+//  within the context of the ActionServlet.
+//  Copyright (C) 2001 Deryl Seale
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You may view the full text here: http://www.gnu.org/copyleft/lesser.txt
+//
+//  If you have any questions or suggestions, you may contact me at
+//  this address: Deryl Seale - deryl@acm.org
+
+package servletunit.struts.tests;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
+import javax.servlet.http.HttpSession;
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionError;
+import org.apache.struts.action.ActionErrors;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionMapping;
+
+public class TokenAction extends Action {
+
+    public ActionForward perform(ActionMapping mapping,
+                                 ActionForm form,
+                                 HttpServletRequest request,
+                                 HttpServletResponse response) {
+
+	ActionErrors errors = new ActionErrors();
+
+	saveToken(request);
+	if (!isTokenValid(request,true)) {
+	    errors.add("password",new ActionError("error.token"));
+	    saveErrors(request,errors);
+        }
+	return mapping.findForward("login");
+    }
+
+}
