5d50238294b832c98d6fdbb6342b0047b0c5fd03
Ben Speakmon <ben.speakmon@gmail.com>
2007-11-06 08:10:57 +0000

    - bug 1088864: implement getResourcePaths(String)

--- src/servletunit/ServletContextSimulator.java
+++ src/servletunit/ServletContextSimulator.java
@@ -528,10 +528,44 @@ public class ServletContextSimulator implements ServletContext
     }
 
     /**
-     * Unsupported in this version.
+     * <p>
+     * Returns a directory-like listing of all the paths to resources within
+     * the web application whose longest sub-path matches the supplied path
+     * argument. Paths indicating subdirectory paths end with a '/'. The
+     * returned paths are all relative to the root of the web application
+     * and have a leading '/'. For example, for a web application containing
+     * </p>
+     *
+     * <p>
+     * /welcome.html<br>
+     * /catalog/index.html<br>
+     * /catalog/products.html<br>
+     * /catalog/offers/books.html<br>
+     * /catalog/offers/music.html<br>
+     * /customer/login.jsp<br>
+     * /WEB-INF/web.xml<br>
+     * /WEB-INF/classes/com.acme.OrderServlet.class,<br>
+     * </p>
+     *
+     * <p>
+     * getResourcePaths("/") returns {"/welcome.html", "/catalog/", "/customer/", "/WEB-INF/"}<br>
+     * getResourcePaths("/catalog/") returns {"/catalog/index.html", "/catalog/products.html", "/catalog/offers/"}.
+     * </p>
+     *
+     * @param path the partial path used to match the resources, which must
+     * start with a /
+     * @return a Set containing the directory listing, or null if there are no
+     * resources in the web application whose path begins with the supplied path.
      */
     public Set getResourcePaths(String path) {
-        throw new UnsupportedOperationException("getResourcePaths operation is not supported!");
+        File file = new File(this.getRealPath(path));
+        String[] fileList = file.list();
+        String prefix = (path.endsWith("/") ? path : path + "/");
+        Set resourcePaths = new HashSet(fileList.length);
+        for (int i = 0; i < fileList.length; i++) {
+            resourcePaths.add(prefix + fileList[i]);
+        }
+        return resourcePaths;
     }
 
     /**
